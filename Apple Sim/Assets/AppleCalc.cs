19f0a219b3c7d5bf3a473730e515b3d65bfaf4a6
yyang2002 <yaweny2@illinois.edu>
2026-03-04 21:21:32 -0600

    added achievement trophies

--- Apple Sim/Assets/AppleCalc.cs
+++ Apple Sim/Assets/AppleCalc.cs
@@ -7,9 +7,36 @@ public class AppleCalc : MonoBehaviour
 
     public TextMeshProUGUI textDisplay;
 
+    [Header("Achievement Trophies")]
+    public GameObject[] trophies;
+    public int[] thresholds;
+    private int achievementLevel = 0;
+    bool[] isActive;
+    public TextMeshProUGUI achievementText;
+
+    void Start()
+    {
+        isActive = new bool[trophies.Length];
+    }
+
     void Update()
     {
         apples += generationRate * Time.deltaTime;
         textDisplay.text = "Apples: " + Mathf.FloorToInt(apples);
+
+        CheckAchievement(Mathf.FloorToInt(apples));
+    }
+
+    void CheckAchievement(int fruit)
+    {
+        if (achievementLevel >= trophies.Length) return;
+        
+        if (!isActive[achievementLevel] && fruit >= thresholds[achievementLevel])
+        {
+            isActive[achievementLevel] = true;
+            trophies[achievementLevel].gameObject.SetActive(true);
+            achievementText.text = "Achievement Unlocked!\n" + thresholds[achievementLevel] + " Fruit Collected";
+            achievementLevel += 1;
+        }
     }
 }
